cf656c5773b4aa818374537176c861970247aacd
Darin London <darin.london@duke.edu>
2021-10-11 23:14:31 -0400

    no longer needs stream file

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,4 @@ fi
 
 helm upgrade mqtt-exporter-${VERSION} helm_chart/mqtt-exporter \
   --atomic --reset-values --debug --wait --timeout "${TIMEOUT:-1m30s}" --install \
-  --set-string mqttExporter.tag=$VERSION \
-  ${CONFIG_FILE:+ --set-file mqttExporter.configFile=$CONFIG_FILE}
+  --set-string mqttExporter.tag=$VERSION
